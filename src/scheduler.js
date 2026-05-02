7bfd41cfe8b5752cc898bb3baca5297000029701
davivpiedade7-source <davivpiedade7@gmail.com>
2026-05-01 23:42:50 -0300

    Update scheduler.js

--- src/scheduler.js
+++ src/scheduler.js
@@ -1,1 +1,25 @@
+// src/scheduler.js
+const cron = require('node-cron');
+
+class Scheduler {
+  constructor() { this.jobs = []; }
+
+  registerDaily(schedules, callback) {
+    schedules.forEach(time => {
+      const [hour, minute] = time.trim().split(':');
+      const expr = `${minute} ${hour} * * *`;
+      if (!cron.validate(expr)) { console.warn('[Scheduler] Horário inválido:', time); return; }
+      const job = cron.schedule(expr, () => {
+        console.log('⏰ Horário atingido:', time);
+        callback();
+      }, { timezone: 'America/Sao_Paulo' });
+      this.jobs.push(job);
+      console.log('⏰ Agendado:', time);
+    });
+  }
+
+  stopAll() { this.jobs.forEach(j => j.stop()); this.jobs = []; }
+}
+
+module.exports = { Scheduler };
 
